77349b4ad375b7bd5f4cae027b07018e887d5e00
Simon Chervenak <sarras305@gmail.com>
2019-11-01 09:07:06 -0400

    doin it

--- rubix.py
+++ rubix.py
@@ -2,7 +2,7 @@ from dataclasses import dataclass
 from enum import EnumMeta, Enum
 from typing import List
 from collections import deque
-from itertools import starmap, combinations, repeat
+from itertools import starmap, combinations, chain, repeat
 
 class Color(Enum):
     RED    = 1
@@ -26,12 +26,11 @@ class Side(Enum):
     RIGHT  = 3
     BOTTOM = 4
 
-class Pos(Enum):
-    A, B, C, D, E, F, G, H, I = range(9)
-
-@dataclass
 class Piece:
-    colors: List[Color]
+    def __init__(self, colors, orientation=None):
+        self.colors = deque(colors)
+        self.orientation = orientation
+        
     
 CORNERS = [[1, 2, 4], [3, 1, 4], [3, 2, 1], [2, 4, 3]]
 
@@ -40,16 +39,24 @@ class Cube:
         self.pieces = []
         self.pieces.extend(map(Piece, map(lambda i: list(map(Color, i)), CORNERS)))
         self.pieces.extend(map(Piece, combinations(Color, 2)))
-        for _ in range(3):
-            self.pieces.extend(map(Piece, map(lambda i: [i], Color)))
+        for i in range(3):
+            for c in Color:
+                for p in [Side.FRONT, Side.LEFT, Side.TOP]:
+                    self.pieces.append(Piece(c, orientation=p))
 
     def move(self, move: Move, level: int, direction: Dir):
         sides = self.get_sides(move)
-        sides.rotate(direction.value)
-        print(sides)
+        if level == 1:
+            piece = next(filter(lambda i: i.colors == sides, self.pieces))
+            piece.colors.rotate(direction.value)
+        else:
+            self.move(move, level=1, direction)
+            
+        # sides.rotate(direction.value)
+        # print(sides)
         
     def get_sides(self, move: Move):
-        return deque([move.value - 1])
+        return deque(CORNERS[move.value - 1])
 
 c = Cube()
 print(c.pieces)
