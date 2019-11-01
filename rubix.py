70cca2b2af8f79140401df265dbc5677f565e916
Simon Chervenak <sarras305@gmail.com>
2019-11-01 16:30:21 -0400

    we out here starting over

--- rubix.py
+++ rubix.py
@@ -14,41 +14,5 @@ class Dir(Enum):
     LEFT  = -1 # also DOWN
     RIGHT = 1 # also UP
 
-CORNERS = [[1, 2, 4], [3, 1, 4], [3, 2, 1], [2, 4, 3]]
-
-class Piece:
-    def __init__(self, colors, orientation=None):
-        self.colors = deque(colors)
-        self.orientation = orientation
-
-        if len(colors) == 2:
-            self.orientation = corner_orientation(colors)
-
-    
-
-class Cube:
-    def __init__(self):
-        self.pieces = []
-        self.pieces.extend(map(Piece, map(lambda i: list(map(Color, i)), CORNERS)))
-        self.pieces.extend(map(Piece, combinations(Color, 2)))
-        for o in center_os[p.value]:
-            for c in Color:
-                self.pieces.append(Piece(c, o))
-
-    def move(self, move: Move, level: int, direction: Dir):
-        sides = self.get_sides(move)
-        if level == 1:
-            piece = next(filter(lambda i: i.colors == sides, self.pieces))
-            piece.colors.rotate(direction.value)
-        else:
-            self.move(move, level=1, direction)
-            centers = filter(lambda i: 
-            
-        # sides.rotate(direction.value)
-        # print(sides)
-    
-    def get_sides(self, move: Move):
-        return deque(CORNERS[move.value - 1])
-
 c = Cube()
 print(c.pieces)
