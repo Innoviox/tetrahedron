bde8b9e4e623468f15ece2e24346569a82bd7a0e
Simon Chervenak <sarras305@gmail.com>
2019-11-01 08:43:43 -0400

    getting somewhere

--- rubix.py
+++ rubix.py
@@ -2,7 +2,7 @@ from dataclasses import dataclass
 from enum import EnumMeta, Enum
 from typing import List
 from collections import deque
-from itertools import starmap
+from itertools import starmap, combinations, repeat
 
 class Color(Enum):
     RED    = 1
@@ -26,25 +26,22 @@ class Side(Enum):
     RIGHT  = 3
     BOTTOM = 4
 
-class Position(Enum):
-    A = 1; B = 2; C = 3
+class Pos(Enum):
+    A, B, C, D, E, F, G, H, I = range(9)
 
 @dataclass
 class Piece:
     colors: List[Color]
-    position: Position
-    side: Side
     
 CORNERS = [[1, 2, 4], [3, 1, 4], [3, 2, 1], [2, 4, 3]]
 
 class Cube:
     def __init__(self):
         self.pieces = []
-        for p in Position:
-            for side
-        # self.pieces.extend(starmap(Piece, zip(CORNERS, Side)))
-        for i in range(3):
-            self.pieces.extend(map(Piece, zip(Side, Side)))
+        self.pieces.extend(map(Piece, map(lambda i: list(map(Color, i)), CORNERS)))
+        self.pieces.extend(map(Piece, combinations(Color, 2)))
+        for _ in range(3):
+            self.pieces.extend(map(Piece, map(lambda i: [i], Color)))
 
     def move(self, move: Move, level: int, direction: Dir):
         sides = self.get_sides(move)
@@ -55,4 +52,4 @@ class Cube:
         return deque([move.value - 1])
 
 c = Cube()
-c.move(Move.TOP, 1, Dir.LEFT)
+print(c.pieces)
