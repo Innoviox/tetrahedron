aaf26e73c30f0f1d75457f6489f79df481719f4f
Simon Chervenak <sarras305@gmail.com>
2019-11-04 21:44:28 -0500

    Update encode.py

--- encode.py
+++ encode.py
@@ -4,12 +4,15 @@ from tqdm import trange
 
 with open("data", "wb") as f:
     spaces, solves = [], []
-    for i in trange(100):
-        t = Tetra()
-        t.random(n=10)
+    for i in trange(10000):
+        try:
+            t = Tetra()
+            t.random(n=5)
 
-        spaces.append(t.to_space())
-        solves.append(t.solve_bfs())
-
+            spaces.append(t.to_space())
+            solves.append(t.solve_bfs())
+        except KeyboardInterrupt:
+            break
+    print("writing")
     pickle.dump([spaces, solves], f)
 
